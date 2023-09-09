a66a3df6c1c751eb18962dccbd97d6c52287781f
mubashirhussen <132121707+mubashirhussen@users.noreply.github.com>
2023-09-09 12:22:13 +0530

    AbstractDemo.java

---
+++ AbstractDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+abstract class Animal{
+    public abstract void animalSound();
+    public void eat(){
+        System.out.println("I CAN EAT");
+    }
+}
+class Cow extends Animal{
+    public void animalSound(){
+        System.out.println("The COw sounds 'MAAA'");
+    }
+}
+public class AbstractDemo{
+    public static void main(String args[])
+    {
+        Cow c = new Cow();
+        c.animalSound();
+        c.eat();
+    }
+}
