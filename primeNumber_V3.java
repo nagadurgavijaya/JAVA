f0a07cbe36e6b8d51707f9551131f4fcc52eba05
mubashirhussen <132121707+mubashirhussen@users.noreply.github.com>
2023-09-14 09:52:42 +0530

    primeNumber_V3.java

---
+++ primeNumber_V3.java
@@ -0,0 +1,19 @@
+//primeNumber_V3.java
+public class PrimeNumber_V3{
+    public static void main(String args[]) {
+        int flag=0,i,num;
+        System.out.println("Enter an integer to check prime or not: ");
+        num=Integer.parseInt(args[0]);
+        for(i=2;i<=num/2;i++){
+            if(num%i==0){
+                System.out.println(num+" is not a prime number. ");
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            System.out.println(num+" is a prime number.");
+        }
+    }
+}
